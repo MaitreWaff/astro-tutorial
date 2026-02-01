12346691273bc1f12e34009b65492ff82e8a2032
MaitreWaff <8455859+MaitreWaff@users.noreply.github.com>
2026-02-01 23:33:48 +0100

    End of Astro Tutorial

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://magical-baklava-7b7b85.netlify.app/",
+  integrations: [preact()]
+});
